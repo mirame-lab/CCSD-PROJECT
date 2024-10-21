c17cd3909cac27d73427406eaa4e54b211d1ca2a
Mirhanieza <73416417+mirame-lab@users.noreply.github.com>
2024-10-21 17:50:28 +0800

    connected data from springboot app to html
    at interiorpackages.html

---
+++ ccsd_project/src/main/java/com/example/ccsd_project/Controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.example.ccsd_project.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    
+
+    public void createOrder(){
+
+    }
+
+}
--- ccsd_project/src/main/java/com/example/ccsd_project/Model/OrderPackage/Order.java
+++ ccsd_project/src/main/java/com/example/ccsd_project/Model/OrderPackage/Order.java
@@ -1,4 +1,21 @@
 package com.example.ccsd_project.Model.OrderPackage;
 
+import com.example.ccsd_project.Model.ServicePackage.Services;
+
 public class Order {
+    // private String id,username;
+    private Services[] servicelist;
+    private double total;
+
+    Order(Services[] sList){
+        this.servicelist = sList;
+    }
+
+    public Services[] getServicelist() {
+        return servicelist;
+    }
+
+    public void setServicelist(Services[] servicelist) {
+        this.servicelist = servicelist;
+    }
 }
