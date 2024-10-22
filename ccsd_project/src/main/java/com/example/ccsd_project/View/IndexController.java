384a255a23b69effbe94aee304bbfb8bb2e3138b
Mirhanieza <73416417+mirame-lab@users.noreply.github.com>
2024-10-22 17:32:59 +0800

    draft landing page + form

---
+++ ccsd_project/src/main/java/com/example/ccsd_project/View/IndexController.java
@@ -0,0 +1,10 @@
+package com.example.ccsd_project.View;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class IndexController {
+
+}
